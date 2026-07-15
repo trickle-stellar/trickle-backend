397447d65596e3c2a77074165551940d2391aeb6
MerlinTheWhiz <michaelnathan505@gmail.com>
2026-07-15 13:22:53 +0100

    Revert "feat: scaffold complete NestJS backend"
    This reverts commit 2610c98adae89d103594a65476a044514871227c.

--- src/common/decorators/api-key.decorator.ts
+++
@@ -1,4 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const IS_API_KEY_KEY = 'isApiKey';
-export const UseApiKey = () => SetMetadata(IS_API_KEY_KEY, true);
--- src/modules/factory/factory.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FactoryController } from './factory.controller';
-import { FactoryService } from './factory.service';
-
-@Module({
-  controllers: [FactoryController],
-  providers: [FactoryService],
-})
-export class FactoryModule {}
